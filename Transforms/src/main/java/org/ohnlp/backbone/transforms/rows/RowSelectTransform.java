99828fa4b60080a8d058c81633c5ab1bcc83ed09
Andrew Wen <2060andrew@gmail.com>
2020-06-25 10:29:53 -0500

    Row selection transform

---
+++ Transforms/src/main/java/org/ohnlp/backbone/transforms/rows/RowSelectTransform.java
@@ -0,0 +1,41 @@
+package org.ohnlp.backbone.transforms.rows;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.beam.sdk.schemas.transforms.Select;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.Row;
+import org.ohnlp.backbone.api.Transform;
+import org.ohnlp.backbone.api.exceptions.ComponentInitializationException;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Transform that performs selection (filtering) of columns within a {@link Row} as well as the option to rename
+ * columns wrapped as a Backbone configuration-compatible element
+ */
+public class RowSelectTransform extends Transform<Row, Row> {
+
+    private Select.Fields<Row> select;
+
+    @Override
+    public void initFromConfig(JsonNode config) throws ComponentInitializationException {
+        List<String> fields = new LinkedList<>();
+        for (JsonNode node : config) {
+            fields.add(node.asText());
+        }
+        this.select
+                = Select.fieldNames(fields.stream().map(s -> s.contains("|") ? s.substring(0, s.indexOf('|')) : s).toArray(String[]::new));
+        fields.forEach(mapping -> {
+            if (mapping.contains("|")) {
+                String[] map = mapping.split("\\|");
+                select = select.withFieldNameAs(map[0], map[1]);
+            }
+        });
+    }
+
+    @Override
+    public PCollection<Row> expand(PCollection<Row> input) {
+        return input.apply(select);
+    }
+}
--- Transforms/src/main/java/org/ohnlp/backbone/transforms/rows/functions/TikaDecoderFunction.java
+++
@@ -1,5 +0,0 @@
-package org.ohnlp.backbone.transforms.rows.functions;
-
-public class TikaDecoderFunction {
-
-}
