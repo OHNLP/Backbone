cf3fd6f1ca9620125c0b6d754757cd39513ec27d
Andrew Wen <2060andrew@gmail.com>
2021-12-30 05:38:16 -0600

    Use offset/fetch equivalent instead of cursor

--- IO/src/main/java/org/ohnlp/backbone/io/jdbc/JDBCExtract.java
+++ IO/src/main/java/org/ohnlp/backbone/io/jdbc/JDBCExtract.java
@@ -2,6 +2,7 @@ package org.ohnlp.backbone.io.jdbc;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import jdk.internal.joptsimple.internal.Strings;
 import org.apache.beam.sdk.io.jdbc.JdbcIO;
 import org.apache.beam.sdk.io.jdbc.SchemaUtilProxy;
 import org.apache.beam.sdk.schemas.Schema;
@@ -13,25 +14,24 @@ import org.apache.beam.sdk.values.Row;
 import org.ohnlp.backbone.api.Extract;
 import org.ohnlp.backbone.api.exceptions.ComponentInitializationException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.sql.*;
+import java.util.*;
 
 /**
  * Performs data extraction using a JDBC connector
- * This class is essentially a configuration wrapper around the beam provided {@link JdbcIO} transform
+ * This class uses {@link JdbcIO} included with Apache Beam to execute parallelized retrieval queries
+ * using offsets for pagination.
  */
 public class JDBCExtract extends Extract {
     private int batchSize;
-    private String cursorName;
     private JdbcIO.DataSourceConfiguration datasourceConfig;
     private String query;
     private long numBatches;
+    private String identifierCol = null;
     private ComboPooledDataSource ds;
+    private String[] orderByCols;
+    private String viewName;
+    private String orderedQuery;
 
     /**
      * Initializes a Beam JdbcIO Provider
@@ -44,9 +44,14 @@ public class JDBCExtract extends Extract {
      *         "driver": "jdbc.driver.class",
      *         "user": "dbUsername",
      *         "password": "dbPassword",
-     *         "query": "query_to_execute_for_extract_task"
+     *         "query": "query_to_execute_for_extract_task",
+     *         "batch_size": integer_batch_size_per_partition_default_1000_if_blank,
+     *         "identifier_col": "column_with_identifier_values"
      *     }
      * </pre>
+     *
+     * By default, batch_size and identifier_col are optional but are highly recommended as the defaults may
+     * not be optimal for performance
      *
      * @param config The configuration section pertaining to this component
      * @throws ComponentInitializationException if an error occurs during initialization or if configuraiton contains
@@ -67,32 +72,76 @@ public class JDBCExtract extends Extract {
             ds.setPassword(password);
             this.datasourceConfig = JdbcIO.DataSourceConfiguration
                     .create(ds);
-            // We will first preflight with a query that creates a cursor for pagination purposes, with a hold for cross-transaction
+            // We will first preflight with a query that counts the number of records so that we can get number
+            // of batches
             String runId = UUID.randomUUID().toString().replaceAll("-", "_");
             //noinspection SqlResolve
-            this.cursorName = "backbone_jdbcio_cursor_" + runId;
-            String cursorQuery = "DECLARE " + cursorName + " WITH HOLD FOR " + query;
             String countQuery = "SELECT COUNT(*) FROM (" + query + ") bckbone_preflight_query_" + runId;
+            this.viewName = "backbone_jdbcextract_" + runId;
+            // Find appropriate columns to order by so that pagination results are consistent
+            this.orderByCols = findPaginationOrderingColumns(this.query);
+            // Get record count so that we know how many batches are going to be needed
             try (Connection conn = ds.getConnection()) {
-                // Ensure count and cursor queries occur in same
-                conn.setAutoCommit(false);
                 ResultSet rs = conn.createStatement().executeQuery(countQuery);
                 rs.next();
                 int resultCount = rs.getInt(1);
-                conn.createStatement().executeQuery(cursorQuery);
                 this.numBatches = Math.round(Math.ceil((double)resultCount/this.batchSize));
-                conn.commit();
             }
-            ds.getConnection().createStatement().execute(cursorQuery);
+            this.orderedQuery = "SELECT * FROM (" + this.query + ") " + this.viewName
+                    + " ORDER BY " + Strings.join(this.orderByCols, ", ") + " ";
+            // Now we have to add the offset/fetch in the dialect local format..
+            // Specifically, postgres and MySQL are special in that they do not conform to the
+            // SQL:2011 standard syntax
+            if (driver.equals("org.postgresql.Driver") || driver.equals("com.mysql.jdbc.Driver")) {
+                this.orderedQuery += "LIMIT " + batchSize + " OFFSET ?";
+            } else { // This is the SQL:2011 standard definition of an offset...fetch syntax
+                this.orderedQuery += "OFFSET ? ROWS FETCH NEXT " + batchSize + " ROWS ONLY";
+            }
         } catch (Throwable t) {
             throw new ComponentInitializationException(t);
         }
     }
 
+    private String[] findPaginationOrderingColumns(String query) throws ComponentInitializationException {
+        try (Connection conn = this.ds.getConnection()) {
+            ResultSetMetaData queryMeta = conn.prepareStatement(query).getMetaData();
+            Map<String, Integer> colNameToIndex = new HashMap<>();
+            for (int i = 0; i < queryMeta.getColumnCount(); i++) {
+                // Assume we are using a case-sensitive impl:
+                // config mismatches can be addressed via config change for identifierCol but same is not true if
+                // impl is case-sensitive and we try to use a case-normalized column name via automatic selection
+                colNameToIndex.put(queryMeta.getColumnLabel(i), i);
+            }
+            if (this.identifierCol != null) {
+                // User-supplied identifier column exists, make sure it actually exists in query results
+                if (!colNameToIndex.containsKey(this.identifierCol)) {
+                    throw new ComponentInitializationException(
+                            new IllegalArgumentException("The supplied identifier_col " + this.identifierCol + " " +
+                            "does not exist in the returned query results. Available columns: " +
+                            Arrays.toString(colNameToIndex.keySet().toArray(new String[0]))));
+                }
+                return new String[] {this.identifierCol};
+            } else {
+                // User did not supply an identifier column, so instead we will concatenate by column index so that
+                // ordering is done for all columns...
+                // Ideally we would want to instead look for a pk/unique constraint and order on that instead of on
+                // everything
+                List<String> colNames = new ArrayList<>();
+                for (int i = 0; i < queryMeta.getColumnCount(); i++) {
+                    colNames.add(queryMeta.getColumnLabel(i));
+                }
+                return colNames.toArray(new String[0]);
+            }
+        } catch (SQLException t) {
+            throw new ComponentInitializationException(t);
+        }
+
+    }
+
     public PCollection<Row> expand(PBegin input) {
-        List<KV<Integer, Integer>> queries = new ArrayList<>();
+        List<Integer> offsets = new ArrayList<>();
         for (int i = 0; i < numBatches; i++) {
-            queries.add(KV.of(batchSize, 1)); // Create a sequence of batches with batchsize in parameter 1
+            offsets.add(i * batchSize); // Create a sequence of batches at the appropriate offset
         }
         Schema schema;
         try (Connection conn = ds.getConnection();
@@ -101,14 +150,14 @@ public class JDBCExtract extends Extract {
         } catch (SQLException throwables) {
             throw new RuntimeException(throwables);
         }
-        return input.apply("JDBC Preflight", Create.of(queries)) // First create partitions # = to num batches
+        return input.apply("JDBC Preflight", Create.of(offsets)) // First create partitions # = to num batches
                 .apply("JDBC Read", // Now actually do the read, the readall function will execute one query per input partition
-                        JdbcIO.<KV<Integer, Integer>, Row>readAll()
+                        JdbcIO.<Integer, Row>readAll()
                                 .withDataSourceConfiguration(datasourceConfig)
-                                .withQuery("FETCH ? FROM " + cursorName)
+                                .withQuery(this.orderedQuery)
                         .withRowMapper(new SchemaUtilProxy.BeamRowMapperProxy(schema))
-                        .withParameterSetter((JdbcIO.PreparedStatementSetter<KV<Integer, Integer>>) (element, preparedStatement) -> {
-                            preparedStatement.setInt(1, batchSize); // We don't actually care about contents
+                        .withParameterSetter((JdbcIO.PreparedStatementSetter<Integer>) (element, preparedStatement) -> {
+                            preparedStatement.setInt(1, element); // Replace
                         })
                 );
     }
