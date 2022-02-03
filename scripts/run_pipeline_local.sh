6da889df0cc7e9c8630569e335a63ecae802fba1
Andrew Wen <2060andrew@gmail.com>
2022-02-03 12:36:20 -0600

    Provide embedded flink run script

--- scripts/run_pipeline_local.sh
+++ scripts/run_pipeline_local.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-# Runs a backbone pipeline locally on a computer. Note that this is not recommended beyond for debugging/testing
-# as it is not scalable and has additional local debugging.
+# Runs a backbone pipeline locally on a computer using an embedded flink cluster
 # GCP or Spark options are recommended for production and/or large-scale use cases
 
 BACKBONE_CONFIG=NAME_OF_CONFIG_TO_USE.json
 
 BACKBONEDIR=$(cd `dirname $0` && pwd)
 cd $BACKBONEDIR
-java -cp bin/Plugin-Manager.jar org.ohnlp.backbone.pluginmanager.PluginManager
 
-BACKBONE_PACKAGED_FILE=bin/Backbone-Core-LocalDebug-Packaged.jar
+FLINK_DIR=flink-1.14.3/bin
+FLINK_EXECUTABLE=$FLINK_DIR/flink
+if [ -f "$FLINK_EXECUTABLE" ]; then
+    echo "Embedded Flink Cluster Already Setup - Skipping New Install"
+else
+    echo "Downloading Apache Flink for Local Run -"
+    wget https://dlcdn.apache.org/flink/flink-1.14.3/flink-1.14.3-bin-scala_2.11.tgz -O flink.tgz
+    tar -xf flink.tgz
+    echo "***Important***: Please adjust default flink settings located at flink-1.14.3/conf/flink-conf.yaml to match your hardware"
+  	echo "Particularly taskmanager.numberOfTaskSlots (generally number of cores * .8 rounded down), "
+    echo "parallelism.default (set equal to number of task slots), "
+  	echo "and taskmanager.memory.process.size (good start is 2GB * number of task slots)"
+    read -p "When done, press [ENTER] to continue"
+fi
+
+BACKBONE_PACKAGED_FILE=bin/Backbone-Core-Flink-Packaged.jar
 if [ -f "$BACKBONE_PACKAGED_FILE" ]; then
-    java -cp bin/Backbone-Core-LocalDebug-Packaged.jar org.ohnlp.backbone.core.BackboneRunner --config=$BACKBONE_CONFIG
+    echo "Starting Embedded Flink Cluster..."
+    $FLINK_DIR/start-cluster.sh
+    echo "Flink Cluster Started - Job Progress Can be Seen via Configured WebUI Port (Default: localhost:8081)"
+    echo "Submitting Job..."
+    $FLINK_DIR/flink run -c org.ohnlp.backbone.core.BackboneRunner bin/Backbone-Core-Flink-Packaged.jar --runner=FlinkRUnner --config=$BACKBONE_CONFIG
+    echo "Job Complete, Shutting Down Embedded Flink Cluster"
+    $FLINK_DIR/stop-cluster.sh
 else
     echo "Packaged backbone installation does not exist. Run package_modules_and_configs for your platform first!"
 fi
