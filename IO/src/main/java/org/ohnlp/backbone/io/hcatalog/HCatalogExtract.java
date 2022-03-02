337cbdd4d67674b0d0ea90ecd3aaff91880dfb60
Andrew Wen <2060andrew@gmail.com>
2022-03-02 01:08:55 -0600

    Add support for HCatalogIO for read portion

---
+++ IO/src/main/java/org/ohnlp/backbone/io/hcatalog/HCatalogExtract.java
@@ -0,0 +1,35 @@
+package org.ohnlp.backbone.io.hcatalog;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.beam.sdk.io.hcatalog.HCatToRow;
+import org.apache.beam.sdk.io.hcatalog.HCatalogIO;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PBegin;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.Row;
+import org.ohnlp.backbone.api.Extract;
+import org.ohnlp.backbone.api.exceptions.ComponentInitializationException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HCatalogExtract extends Extract {
+    Map<String, String> configProperties;
+    private String database;
+    private String table;
+
+    @Override
+    public void initFromConfig(JsonNode config) throws ComponentInitializationException {
+        this.configProperties = new HashMap<>();
+        this.configProperties.put("hive.metastore.uris", config.get("metastore_uris").asText());
+        this.database = config.get("database").asText();
+        this.table = config.get("table").asText();
+    }
+
+    @Override
+    public PCollection<Row> expand(PBegin input) {
+        return HCatToRow.fromSpec(HCatalogIO.read().withConfigProperties(configProperties)
+                .withDatabase(database)
+                .withTable(table)).expand(input);
+    }
+}
