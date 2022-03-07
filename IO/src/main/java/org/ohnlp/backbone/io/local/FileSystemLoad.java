6bd36d78962f30567efd84cee64776f6caccfe7c
Nishanth Pavinkurve <npp267@nyu.edu>
2022-03-07 12:20:27 -0500

    Support streaming writes to JSONL file

--- IO/src/main/java/org/ohnlp/backbone/io/local/FileSystemLoad.java
+++ IO/src/main/java/org/ohnlp/backbone/io/local/FileSystemLoad.java
@@ -1,19 +1,21 @@
 package org.ohnlp.backbone.io.local;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import org.apache.beam.sdk.io.FileIO;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PDone;
 import org.apache.beam.sdk.values.Row;
-import org.apache.commons.text.StringEscapeUtils;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.transforms.ToJson;
+import org.apache.beam.sdk.transforms.windowing.AfterFirst;
+import org.apache.beam.sdk.transforms.windowing.AfterPane;
+import org.apache.beam.sdk.transforms.windowing.AfterProcessingTime;
+import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.Repeatedly;
+import org.apache.beam.sdk.transforms.windowing.Window;
+import org.joda.time.Duration;
+
 import org.ohnlp.backbone.api.Load;
 import org.ohnlp.backbone.api.exceptions.ComponentInitializationException;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.channels.Channels;
-import java.nio.channels.WritableByteChannel;
-import java.util.stream.Collectors;
 
 /**
  * Reads in records to a file system directory.
@@ -30,43 +32,36 @@ import java.util.stream.Collectors;
  */
 public class FileSystemLoad extends Load {
 
-    private FileIO.Write<Void, Row> writer;
+  private String filenamePrefix;
+  private int maxOutputPartitionMinutes;
+  private int outputPartitionMinutesDelay;
+  private int outputPartitionLateness;
+  private int batchSize;
 
     @Override
     public void initFromConfig(JsonNode config) throws ComponentInitializationException {
-        this.writer = FileIO.<Row>write()
-                .via(new CSVSink())
-                .to(config.get("fileSystemPath").asText())
-                .withPrefix("backbone-out-")
-                .withSuffix(".csv");
+        this.filenamePrefix = config.get("fileSystemPath").asText() + "/backbone-out-";
+        this.maxOutputPartitionMinutes = config.get("maxOutputPartitionMinutes").asInt();
+        this.outputPartitionMinutesDelay = this.maxOutputPartitionMinutes/10;
+        this.outputPartitionLateness = this.maxOutputPartitionMinutes/5;
+        this.batchSize = config.get("batchSize").asInt();
     }
 
     @Override
     public PDone expand(PCollection<Row> input) {
-
-        input.apply(writer);
+        input
+          .apply("Serialize to Json", ToJson.of())
+          .apply("Windowing", 
+              Window.<String>into(FixedWindows.of(Duration.standardMinutes(this.maxOutputPartitionMinutes)))
+                  .triggering(Repeatedly.forever(
+                            AfterFirst.of(AfterPane.elementCountAtLeast(this.batchSize),
+                                  AfterProcessingTime
+                                        .pastFirstElementInPane()
+                                        .plusDelayOf(Duration.standardMinutes(this.outputPartitionMinutesDelay)))))
+                  .withAllowedLateness(Duration.standardSeconds(this.outputPartitionLateness))
+                  .discardingFiredPanes())
+          .apply(TextIO.write().to(this.filenamePrefix).withWindowedWrites().withNumShards(1).withSuffix(".txt"));
         return PDone.in(input.getPipeline());
     }
 
-    private static class CSVSink implements FileIO.Sink<Row> {
-        private PrintWriter writer;
-        private boolean wroteHeader;
-
-        public void open(WritableByteChannel channel) throws IOException {
-            writer = new PrintWriter(Channels.newOutputStream(channel));
-            wroteHeader = false;
-        }
-
-        public void write(Row element) throws IOException {
-            if (!wroteHeader) {
-                writer.println(element.getSchema().getFieldNames().stream().map(StringEscapeUtils::escapeCsv).collect(Collectors.joining(",")));
-                wroteHeader = true;
-            }
-            writer.println(element.getValues().stream().map(o -> StringEscapeUtils.escapeCsv(o.toString())).collect(Collectors.joining(",")));
-        }
-
-        public void flush() throws IOException {
-            writer.flush();
-        }
-    }
 }
