8fafa1b7ef3b4f765495fbedc0d21c4fbb222dfa
Andrew Wen <Wen.Andrew@mayo.edu>
2023-03-09 12:52:39 -0600

    Use String FQN for requirements dec to avoid unnecessary imports

--- API/src/main/java/org/ohnlp/backbone/api/annotations/ComponentDescription.java
+++ API/src/main/java/org/ohnlp/backbone/api/annotations/ComponentDescription.java
@@ -1,11 +1,7 @@
 package org.ohnlp.backbone.api.annotations;
 
-import org.ohnlp.backbone.api.BackbonePipelineComponent;
-
-import java.lang.reflect.Array;
-
 public @interface ComponentDescription {
     String name();
     String desc();
-    Class<? extends BackbonePipelineComponent<?,?>>[] requires() default {};
+    String[] requires() default {};
 }
