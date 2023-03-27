3778f9b33b910b9ea8ce812921f768e0454a3b9b
Andrew Wen <Wen.Andrew@mayo.edu>
2023-03-27 15:01:55 -0500

    Component Descriptions should be retained at runtime

--- API/src/main/java/org/ohnlp/backbone/api/annotations/ComponentDescription.java
+++ API/src/main/java/org/ohnlp/backbone/api/annotations/ComponentDescription.java
@@ -1,5 +1,12 @@
 package org.ohnlp.backbone.api.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ComponentDescription {
     String name();
     String desc();
