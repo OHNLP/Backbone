b7af803a5565a102197cc1071cd7c231b2e621f5
Andrew Wen <Wen.Andrew@mayo.edu>
2023-05-25 11:25:53 -0500

    Watch service monitors directories, not files

--- API/src/main/java/org/ohnlp/backbone/api/components/xlang/python/PythonBridge.java
+++ API/src/main/java/org/ohnlp/backbone/api/components/xlang/python/PythonBridge.java
@@ -2,9 +2,6 @@ package org.ohnlp.backbone.api.components.xlang.python;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.ExecuteException;
@@ -16,10 +13,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.Serializable;
 import java.net.InetAddress;
-import java.nio.file.FileSystems;
-import java.nio.file.WatchService;
-import java.security.SecureRandom;
+import java.nio.file.*;
+import java.util.Arrays;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
@@ -28,6 +25,7 @@ import java.util.zip.ZipInputStream;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 
 public class PythonBridge<T> implements Serializable {
+    private final long pythonInitTimeout = 30000; // TODO make this configurable
     private final String bundleName;
     private final String entryPoint;
     private final Class<T> pythonEntryPointClass;
@@ -59,22 +57,25 @@ public class PythonBridge<T> implements Serializable {
         File tmpDir = new File(System.getProperty("java.io.tmpdir"));
         this.envDir = new File(tmpDir, name);
         this.envDir.mkdirs();
-        try (ZipInputStream zis = new ZipInputStream(getClass().getResourceAsStream("/python_modules/" + this.bundleName))) {
-            ZipEntry entry;
-            while ((entry = zis.getNextEntry()) != null) {
-                if (entry.isDirectory()) {
-                    continue;
+        // Extract both the python launcher script and the bundle itself
+        for (String bundle : Arrays.asList("backbone_python_launcher.zip", this.bundleName)) {
+            try (ZipInputStream zis = new ZipInputStream(getClass().getResourceAsStream("/python_modules/" + bundle))) {
+                ZipEntry entry;
+                while ((entry = zis.getNextEntry()) != null) {
+                    if (entry.isDirectory()) {
+                        continue;
+                    }
+                    String pathRelative = entry.getName();
+                    File pathInTmp = new File(this.envDir, pathRelative);
+                    byte[] contents = zis.readAllBytes();
+                    try (FileOutputStream fos = new FileOutputStream(pathInTmp)) {
+                        fos.write(contents);
+                        fos.flush();
+                    }
                 }
-                String pathRelative = entry.getName();
-                File pathInTmp = new File(this.envDir, pathRelative);
-                byte[] contents = zis.readAllBytes();
-                try (FileOutputStream fos = new FileOutputStream(pathInTmp)) {
-                    fos.write(contents);
-                    fos.flush();
-                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 
@@ -85,7 +86,7 @@ public class PythonBridge<T> implements Serializable {
         // Create sentinel watcher for python process initialization
         WatchService watcher = FileSystems.getDefault().newWatchService();
         File done = new File(this.envDir, "python_bridge_meta.done");
-        done.toPath().register(watcher, ENTRY_CREATE);
+        this.envDir.toPath().register(watcher, ENTRY_CREATE);
         // Launch the python process
         String cmd = String.join(" ", new File("bin", "python").getAbsolutePath(), "backbone_module_launcher.py", entryPoint);
         CommandLine cmdLine = CommandLine.parse(cmd);
@@ -100,6 +101,9 @@ public class PythonBridge<T> implements Serializable {
 
                 @Override
                 public void onProcessFailed(ExecuteException e) {
+                    try {
+                        shutdownBridge();
+                    } catch (Throwable ignored) {}
                     throw new RuntimeException("Broken Python Bridge", e);
                 }
             });
@@ -107,18 +111,42 @@ public class PythonBridge<T> implements Serializable {
             throw new RuntimeException(e);
         }
         // Wait for python process to start and the python_bridge_meta.done file to be created
-        try {
-            watcher.take();
-        } catch (InterruptedException e) {
-            throw new RuntimeException("Failed to start python bridge", e);
+        long start = System.currentTimeMillis();
+        boolean initDone = false;
+        while (!initDone) {
+            if (System.currentTimeMillis() - start > pythonInitTimeout) {
+                try {
+                    shutdownBridge();
+                } catch (Throwable ignored) {}
+                throw new IOException("Failed to start python process within timeout period");
+            }
+            try {
+                WatchKey wk = watcher.poll(25, TimeUnit.MILLISECONDS);
+                if (wk != null) {
+                    for (WatchEvent<?> e : wk.pollEvents()) {
+                        Path createdFilePath = ((WatchEvent<Path>) e).context();
+                        if (createdFilePath.endsWith("python_bridge_meta.done")) {
+                            initDone = true;
+                            break;
+                        }
+                    }
+                }
+            } catch (InterruptedException e) {
+                try {
+                    shutdownBridge();
+                } catch (Throwable ignored) {}
+                throw new RuntimeException("Failed to start python bridge", e);
+            }
         }
 
         // Initiate java-side client server with info passed in the created python meta
         JsonNode bridgeMeta = new ObjectMapper().readTree(new File(this.envDir, "python_bridge_meta.json"));
         this.bridgeServer = new ClientServer.ClientServerBuilder()
                 .authToken(bridgeMeta.get("token").asText())
-                .javaPort(bridgeMeta.get("port").asInt())
+                .javaPort(bridgeMeta.get("java_port").asInt())
                 .javaAddress(InetAddress.getLoopbackAddress())
+                .pythonPort(bridgeMeta.get("python_port").asInt())
+                .pythonAddress(InetAddress.getLoopbackAddress())
                 .build();
         this.bridgeServer.startServer();
         Logger.getGlobal().log(Level.INFO, "Successfully started python binding for " + entryPoint + " on " + InetAddress.getLoopbackAddress() + " port " + bridgeMeta.get("port").asInt());
