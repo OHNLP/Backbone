cc2f442127ee17be3b6f7dc832f310b74c4d67af
Andrew Wen <Andrew.Wen@uth.tmc.edu>
2023-07-26 13:46:36 -0500

    Fix missing param in PythonRow#get_value

--- API/src/main/java/org/ohnlp/backbone/api/components/xlang/python/PythonRow.java
+++ API/src/main/java/org/ohnlp/backbone/api/components/xlang/python/PythonRow.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface PythonRow {
     PythonSchema get_schema();
-    Object get_value();
+    Object get_value(String column);
     List<Object> get_values();
 
 }
