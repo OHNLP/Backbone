148b0c1d805b7fa6f3ea769e560e2ad07f10607e
Andrew Wen <Andrew.Wen@uth.tmc.edu>
2023-07-28 16:44:19 -0500

    PythonRow missing set_value interface

--- API/src/main/java/org/ohnlp/backbone/api/components/xlang/python/PythonRow.java
+++ API/src/main/java/org/ohnlp/backbone/api/components/xlang/python/PythonRow.java
@@ -6,5 +6,6 @@ public interface PythonRow {
     PythonSchema get_schema();
     Object get_value(String column);
     List<Object> get_values();
+    void set_value(String colName, Object value);
 
 }
